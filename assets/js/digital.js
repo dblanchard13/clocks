0687571788e249971fcd88f32a2c78151e3cb50f
dblanchard13 <davidb.prof@gmail.com>
2015-06-20 01:34:32 -0700

    Added basic music controls for convenience

--- assets/js/digital.js
+++ assets/js/digital.js
@@ -16,4 +16,27 @@
 
   song.play();
 
+  $('.play-container').on('click', function(){
+    if(song.paused){
+      $('.play-container').find('i').text('pause');
+      song.play();
+    }
+    else {
+      $('.play-container').find('i').text('play_arrow');
+      song.pause();
+    }
+  });
+
+
+  $('.mute-container').on('click', function(){
+    if(song.volume > 0){
+      $('.mute-container').find('i').text('volume_off');
+      song.volume = 0;
+    }
+    else {
+      $('.mute-container').find('i').text('volume_up');
+      song.volume = 0.3;
+    }
+  })
+
 }());
