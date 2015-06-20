487f9387ea004f4ca3967e03b2b2ebcba9867bae
dblanchard13 <davidb.prof@gmail.com>
2015-06-20 00:42:59 -0700

    Turns out I'd gotten over ambitious trying to upload the other track so switched it out for dayAndAllNight

--- assets/js/digital.js
+++ assets/js/digital.js
@@ -6,8 +6,14 @@
     $('.container.alarm').find('span').html(time.toLocaleTimeString())
   }, 500)
 
-  var song = new Audio('assets/brendanFallis.mp3');
+  var song = new Audio('assets/dayAndAllNight.mp3');
   song.volume = 0.3;
+
+  $(song).bind('ended', function()  {
+      song.currentTime = 0;
+      song.play();
+  });
+
   song.play();
 
 }());
